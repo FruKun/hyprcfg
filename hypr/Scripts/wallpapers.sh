4fbcff8a498941f14d2109d6bd4fe72a40d9066d
frukun <peromata@yandex.ru>
2025-02-05 23:05:37 +0500

    update

--- hypr/Scripts/wallpapers.sh
+++ hypr/Scripts/wallpapers.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-swaybg -i $(find ~/wallpapers/. -type f | shuf -n1) -m fill &
+swaybg -i $(find ~/wallpapers/img/. -type f | shuf -n1) -m fill &
 OLD_PID=$!
 while true; do
     sleep 1200
-    swaybg -i $(find ~/wallpapers/. -type f | shuf -n1) -m fill &
+    swaybg -i $(find ~/wallpapers/img/. -type f | shuf -n1) -m fill &
     NEXT_PID=$!
     sleep 5
     kill $OLD_PID
